ae2927648fd2226ed6a4ba7eac5e7c40a53c8f92
Isaiah Wright <icwright902@gmail.com>
2021-02-02 11:55:29 -0500

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+import {LEVEL, OBJECT_TYPE} from './setup';
+
+//Dom Element
+const gameGrid = document.querySelector('#game');
+const scoreTable = document.querySelector('#score');
+const scoreTable = document.querySelector('#start-button');
+
+//Game constants
+const POWER_PILL_TIME  = 10000;
+const GLOBAL_SPEED = 80; 
+
+//Inital Setup
+let score =0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
