951176fe8b4674295664e4ebe0b71c3b0407f22d
Isaiah Wright <icwright902@gmail.com>
2021-02-03 13:46:22 -0500

    game finished :)

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import {randomMovement} from './ghostMoves';
 import GameBoard from './GameBoard';
 import Pacman from './Pacman';
 import Ghost from './Ghost';
+import soundDot from './sounds/munch.wav';
+import soundPill from './sounds/pill.wav';
+import soundGameStart from './sounds/game_start.wav';
+import soundGameOver from './sounds/death.wav';
+import soundGhost from './sounds/eat_ghost.wav';
+
 
 //Dom Element
 const gameGrid = document.querySelector('#game');
@@ -21,7 +27,14 @@ let gameWin = false;
 let powerPillActive = false;
 let powerPillTimer = null;
 
+//Audio
+function playAudio(audio){
+    const soundEffect = new Audio(audio);
+    soundEffect.play();
+}
+
 function gameOver(pacman, grid){
+    playAudio(soundGameOver);
     document.removeEventListener('keydown', e=>
     pacman.handleKeyInput(e, gameBoard.objectExist)
     );
@@ -40,6 +53,7 @@ function checkCollision(pacman, ghosts){
 
  if(collidedGhost){
      if(pacman.powerPill){
+         playAudio(soundGhost);
         gameBoard.removeObject(collidedGhost.pos, [
             OBJECT_TYPE.GHOST,
             OBJECT_TYPE.SCARED,
@@ -64,12 +78,14 @@ function gameLoop(pacman, ghosts){
 
    //Check if Pacman eats a dot
    if(gameBoard.objectExist(pacman.pos, OBJECT_TYPE.DOT)){
+       playAudio(soundDot);
        gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.DOT]);
        gameBoard.dotCount--;
        score += 10;
    }
    //check if pacman eats powerpill
    if(gameBoard.objectExist(pacman.pos, OBJECT_TYPE.PILL)){
+       playAudio(soundPill);
        gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.PILL]);
 
        pacman.powerPill = true;
@@ -99,6 +115,7 @@ function gameLoop(pacman, ghosts){
 }
 
 function startGame(){
+    playAudio(soundGameStart);
 gameWin = false;
 powerPillActive = false;
 score = 0;
