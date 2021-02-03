57d0e56ba15512bdddbbdca750d5bef73a315c68
Isaiah Wright <icwright902@gmail.com>
2021-02-03 13:37:57 -0500

    game fully functional

--- index.js
+++ index.js
@@ -61,6 +61,41 @@ function gameLoop(pacman, ghosts){
 
    ghosts.forEach((ghost) => gameBoard.moveCharacter(ghost));
    checkCollision(pacman, ghosts);
+
+   //Check if Pacman eats a dot
+   if(gameBoard.objectExist(pacman.pos, OBJECT_TYPE.DOT)){
+       gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.DOT]);
+       gameBoard.dotCount--;
+       score += 10;
+   }
+   //check if pacman eats powerpill
+   if(gameBoard.objectExist(pacman.pos, OBJECT_TYPE.PILL)){
+       gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.PILL]);
+
+       pacman.powerPill = true;
+       score += 50;
+
+       clearTimeout(powerPillTimer);
+       powerPillTimer = setTimeout(  
+           ()=> (pacman.powerPill = false), 
+           POWER_PILL_TIME
+           );
+   }
+
+   //Change ghost scare mode depending on power pill
+   if(pacman.powerPill !== powerPillActive){
+       powerPillActive = pacman.powerPill;
+       ghosts.forEach((ghost) => (ghost.isScared = pacman.powerPill));
+   }
+
+   //Check if all dots have been eaten
+   if(gameBoard.dotCount === 0){
+       gameWin = true;
+       gameOver(pacman, ghosts);
+   }
+
+   // Show the score
+   scoreTable.innerHTML = score;
 }
 
 function startGame(){
